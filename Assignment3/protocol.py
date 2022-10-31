4f598638df7268e2dcd3ce88af49ce3f797e235e
Long Nguyen <longnguyen2001@gmail.com>
2022-10-31 16:16:40 -0700

    switch from EAX to CBC mode

--- Assignment3/protocol.py
+++ Assignment3/protocol.py
@@ -9,12 +9,6 @@ CLIENT = b"CLNT"
 SERVER = b"SRVR"
 SEPERATOR = b"SEPERATORLHKAJSHFKUHDSKJFFK"
 
-# # https://crypto.stackexchange.com/questions/67797/in-diffie-hellman-are-g-and-p-universal-constants-or-are-they-chosen-by-one
-# p = 0xfca682ce8e12caba26efccf7110e526db078b05edecbcd1eb4a208f3ae1617ae01f35b91a47e6df63413c5e12ed0899bcd132acd50d99151bdc43ee737592e17
-# g = 0x678471b27a9cf44ee91a49c5147db1a9aaf244f05a434d6486931d2d14271b9e35030b71fd73da179069b32e2935630e1c2062354d0da20a6c416e50be794ca4
-# p = 0xfca682ce8e12caba26efccf7110e526db078b05edecbcd1eb4a
-# g = 0x678471b27a9cf44ee91a49c5147db1a9aaf244f05a434
-
 # https://asecuritysite.com/encryption/getprimen
 # Generated 128 bit prime numbers
 p = 256282428810585846751330807206456834043
@@ -62,11 +56,11 @@ class Protocol:
 
             challenge_response = SERVER + SEPERATOR + Ra + SEPERATOR + gbmodp.to_bytes(length=16, byteorder='big')
 
-            cipher = AES.new(self.hash_shared_secret(shared_secret), AES.MODE_EAX)
-            nonce = cipher.nonce
-            ciphertext, tag = cipher.encrypt_and_digest(challenge_response)
+            cipher = AES.new(self.hash_shared_secret(shared_secret), AES.MODE_CBC)
+            iv = cipher.iv
+            ciphertext = cipher.encrypt(pad(challenge_response, 16))
             
-            return_message = PROTOCOL + SEPERATOR + self.challenge + SEPERATOR + ciphertext + SEPERATOR + nonce
+            return_message = PROTOCOL + SEPERATOR + self.challenge + SEPERATOR + ciphertext + SEPERATOR + iv
             return return_message
 
         # Alice receives bob's challenge response and bob's challenge
@@ -74,9 +68,9 @@ class Protocol:
             print("Inside case 2")
             ciphertext = message_parts[2]
 
-            cipher = AES.new(self.hash_shared_secret(shared_secret), AES.MODE_EAX, nonce=message_parts[3])
+            cipher = AES.new(self.hash_shared_secret(shared_secret), AES.MODE_CBC, iv=message_parts[3])
 
-            plaintext = cipher.decrypt(ciphertext)
+            plaintext = unpad(cipher.decrypt(ciphertext), 16)
             plaintext_parts = plaintext.split(SEPERATOR)
 
             # print(plaintext_parts)
@@ -94,12 +88,12 @@ class Protocol:
                 # challenge_response = f"{CLIENT},{Rb},{gamodp}"
                 challenge_response = CLIENT + SEPERATOR + Rb + SEPERATOR + gamodp.to_bytes(length=16, byteorder='big')
                 
-                cipher = AES.new(self.hash_shared_secret(shared_secret), AES.MODE_EAX)
-                nonce = cipher.nonce
-                ciphertext = cipher.encrypt(challenge_response)
+                cipher = AES.new(self.hash_shared_secret(shared_secret), AES.MODE_CBC)
+                iv = cipher.iv
+                ciphertext = cipher.encrypt(pad(challenge_response, 16))
 
                 # return_message = f"{PROTOCOL}{SEPERATOR}{ciphertext}"
-                return_message = PROTOCOL + SEPERATOR + ciphertext + SEPERATOR + nonce
+                return_message = PROTOCOL + SEPERATOR + ciphertext + SEPERATOR + iv
 
                 self.SetSessionKey((int.from_bytes(gbmodp, byteorder='big')^self.diffie_hellman_const) % p)
 
@@ -111,8 +105,8 @@ class Protocol:
             print("Inside case 3")
             ciphertext = message_parts[1]
 
-            cipher = AES.new(self.hash_shared_secret(shared_secret), AES.MODE_EAX, nonce=message_parts[2])
-            plaintext = cipher.decrypt(ciphertext)
+            cipher = AES.new(self.hash_shared_secret(shared_secret), AES.MODE_CBC, iv=message_parts[2])
+            plaintext = unpad(cipher.decrypt(ciphertext), 16)
             plaintext_parts = plaintext.split(SEPERATOR)
 
             if plaintext_parts[0] == CLIENT:
@@ -140,11 +134,11 @@ class Protocol:
     # RETURN AN ERROR MESSAGE IF INTEGRITY VERITIFCATION OR AUTHENTICATION FAILS
     def EncryptAndProtectMessage(self, plain_text):
         data = plain_text.encode("utf-8")
-        cipher = AES.new(self._key, AES.MODE_EAX)
-        cipher_text, tag = cipher.encrypt_and_digest(data)
+        cipher = AES.new(self._key, AES.MODE_CBC)
+        cipher_text = cipher.encrypt(pad(data, 16))
         hmac = HMAC.new(self._key, digestmod=SHA256) 
         hmac.update(cipher_text)
-        return cipher.nonce + SEPERATOR + cipher_text + SEPERATOR + hmac.digest()
+        return cipher.iv + SEPERATOR + cipher_text + SEPERATOR + hmac.digest()
 
 
     # Decrypting and verifying messages
@@ -152,7 +146,7 @@ class Protocol:
     # RETURN AN ERROR MESSAGE IF INTEGRITY VERITIFCATION OR AUTHENTICATION FAILS
     def DecryptAndVerifyMessage(self, cipher_text):
         hmac = HMAC.new(self._key, digestmod=SHA256)
-        nonce, data, mac = cipher_text.split(SEPERATOR)
+        iv, data, mac = cipher_text.split(SEPERATOR)
         try: 
             hmac.update(data)
             hmac.verify(mac)
@@ -160,8 +154,8 @@ class Protocol:
         except ValueError: 
             return "Integrity check failed"
         try: 
-            cipher = AES.new(self._key, AES.MODE_EAX, nonce)
-            plain_text = cipher.decrypt(data)
+            cipher = AES.new(self._key, AES.MODE_CBC, iv)
+            plain_text = unpad(cipher.decrypt(data), 16)
         except (ValueError, KeyError):
             return "Incorrect decryption error"
         return plain_text
